48f0404b5706bb877a52460c79f9e37a701153a0
CorentinPolard <corentinpolard@gmail.com>
2025-01-17 14:29:56 +0100

    Résolution de l'exercice 6

---
+++ Exercice6/exercice6.js
@@ -0,0 +1,9 @@
+function afficheMultiplication(nbr) {
+    for (let i = 1; i <=12; i++) {
+        console.log(`${nbr}x${i} = ${nbr*i}`);
+    }
+    return;
+}
+
+afficheMultiplication(5);
+afficheMultiplication(10);

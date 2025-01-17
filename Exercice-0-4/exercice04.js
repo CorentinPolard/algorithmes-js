cb315858295e78ed0f5bcb198f302060710c8771
CorentinPolard <corentinpolard@gmail.com>
2025-01-17 11:51:28 +0100

    Résolution de l'exercice  0.4

---
+++ Exercice-0-4/exercice04.js
@@ -0,0 +1,9 @@
+let height = 112;
+let age = 21;
+
+if (age > 18 && height > 140) {
+    console.log("Je peux rentrer dans le manège.");
+}
+else {
+    console.log("Je n'ai pas le droit de rentrer dans le manège.");
+}

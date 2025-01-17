fb7bd3fb56bd92fed0a8597cabd6d1dcde8b5ef5
CorentinPolard <corentinpolard@gmail.com>
2025-01-17 11:42:46 +0100

    Résolution de l'exercice 0.3

---
+++ Exercice-0-3/exercice03.js
@@ -0,0 +1,10 @@
+let gender = "male";
+let height = 176;
+let age = 21;
+
+if (gender === "male") {
+    console.log(`J'ai ${age} ans, je mesure ${height} centimètres et suis un homme.`)
+}
+else {
+    console.log(`J'ai ${age} ans, je mesure ${height} centimètres et suis une femme.`)
+}
